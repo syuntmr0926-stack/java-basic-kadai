bf407ab865000fa0b685a428920e3b1ed2920607
taiki.coming <syun.tmr0926@gmail.com>
2025-10-15 23:18:33 +0900

    Add files via upload

---
+++ kadai_018/KatoExec_Chapter18.java
@@ -0,0 +1,16 @@
+package kadai_018;
+
+public class KatoExec_Chapter18 {
+	
+	public static void main(String[] args) {
+
+        KatoTaro_Chapter18 katoTaro = new KatoTaro_Chapter18();
+        KatoIchiro_Chapter18 katoIchiro = new KatoIchiro_Chapter18();
+        KatoHanako_Chapter18 katoHanako = new KatoHanako_Chapter18();
+
+        katoTaro.execIntroduce();
+        katoIchiro.execIntroduce();
+        katoHanako.execIntroduce();
+    }
+
+}
